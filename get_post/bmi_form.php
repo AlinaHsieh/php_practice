5bf90f0475c4fc06b43f68419dc7f3a02e1a1648
Alina Hsieh <ahsieh74@gmail.com>
2023-05-13 17:15:52 +0800

    BMI表單傳值練習（bmi_form跟跟result）

---
+++ get_post/bmi_form.php
@@ -0,0 +1,11 @@
+<h1>BMI計算</h1>
+<form action="result.php" method="get">
+
+<label for="height">身高(公分)</label>    
+<input type="number" name="height" id="height"><br>
+
+<label for="weight">體重(公斤)</label>    
+<input type="number" name="weight" id="weight"><br>
+
+<input type="submit" value="計算BMI">
+</form>
