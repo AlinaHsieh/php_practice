45430157eb9b976d6b19ca0e456610b0501a9716
Alina Hsieh <ahsieh74@gmail.com>
2023-06-18 18:06:28 +0800

    function自訂函式(進階)

--- function.php
+++ function.php
@@ -120,20 +120,123 @@ echo $sum2(10,20);
 //////////////////////////////////////////////////
 //db function
 
+function dd($ary){
+    echo "<pre>";
+    print_r($ary);
+    echo "</pre>";
+}
+
+function pdo(){
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    $pdo = new PDO($dsn, 'root', '');
+    return $pdo;
+}
 
 //all()-給定資料表名後，會回傳整個資料表的資料
 function all($table){
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
-    $pdo = new PDO($dsn, 'root', '');
+    // $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    // $pdo = new PDO($dsn, 'root', '');
+    $pdo=pdo();
     $sql = "select * from $table";
     $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
 }
 
+/* 
+寫一個可以處理比較複雜條件的all function
+ * all($table) => 全部資料表的內容
+ * 例:select * from `topics` => all('topics')
+ * ---------------------------------------------------------------
+ * all($table,$array) => 以and為基礎的符合條件資料
+ * 例: select * from `topics` where `type`='1' && `login`=1; => all('topics',['type'=>1,'login'=>1]) ;
+ * ---------------------------------------------------------------
+ * all($table,$sql) => 以sql字串為條件的資料
+ * 例: select * from `topics` where open_time <= '2023/06/02' order by `id` desc
+ * all(`topcis`,"where open_time <= '2023/06/02' order by `id` desc")
+ * ---------------------------------------------------------------
+ * all($table,$array,$sql) => 符合複雜條件的資料
+ * 例: select * from `topics` where `type`=1 && `login`=1  order by `id` desc
+ * all(`topcis`,['type'=>1,,'login'=>1], " order by `id` desc")
+ */
+function newAll($table,...$arg){ //判斷$arg是否有值
+    $pdo=pdo();
+    $sql = "select * from $table ";
+    if(!empty($arg)){            //不是空的:可能是陣列、可能是字串
+        if(is_array($arg[0])){   //如果是陣列，從$arg的key值0的位置開始判斷
+            foreach($arg[0] as $key => $value ){
+                $tmp[]="`$key`='$value'";
+            }
+            $sql = $sql . "where" . join(" && ",$tmp);
+        }else{
+            $sql = $sql . "where" . $arg[0];  //如果是字串，則原本sql語法+$arg[0]的東西
+        }
+    }
+    if(isset($arg[1])){   //判斷是否有第三個參數(預設是sql語句)存在 all($table,$array,$sql) 
+        $sql = $sql ."where" . $arg[1];
+    }
+
+    $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+}
+
+
+//計數用的函式
+function _count($table,...$arg){ 
+    $pdo=pdo();
+    $sql = "select count(*) from $table ";
+    if(!empty($arg)){            
+        if(is_array($arg[0])){   
+            foreach($arg[0] as $key => $value ){
+                $tmp[]="`$key`='$value'";
+            }
+            $sql = $sql ."where" . join(" && ",$tmp);
+        }else{
+            $sql = $sql ."where" . $arg[0];  
+        }
+    }
+    if(isset($arg[1])){    
+        $sql = $sql ."where" . $arg[1];
+    }
+
+    $rows = $pdo->query($sql)->fetchColumn();
+    return $rows;
+}
+// echo _count('topics');
+
+
+//計數用的函式(方法)
+function math($table,$math,$col,...$arg){ 
+    $pdo=pdo();
+    $sql = "select $math(`$col`) from $table ";
+    if(!empty($arg)){            
+        if(is_array($arg[0])){   
+            foreach($arg[0] as $key => $value ){
+                $tmp[]="`$key`='$value'";
+            }
+            $sql = $sql . "where" . join(" && ",$tmp);
+        }else{
+            $sql = $sql . "where" .  $arg[0];  
+        }
+    }
+    if(isset($arg[1])){    
+        $sql = $sql . "where" .  $arg[1];
+    }
+
+    $rows = $pdo->query($sql)->fetchColumn();
+    return $rows;
+}
+echo math('options','min','id');
+echo "<br>";
+echo math('options','sum','total',['subject_id'=>8]);
+
+
+
+
 //find()-會回傳資料表指定id的資料
 function find($table, $id){
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
-    $pdo = new PDO($dsn, 'root', '');
+    // $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    // $pdo = new PDO($dsn, 'root', '');
+    $pdo=pdo();
     $sql = "select * from $table where `id` = $id";
     $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     return $row;
@@ -141,8 +244,9 @@ function find($table, $id){
 
 //以function find()舉例，當今天條件可能為多筆時:加判斷式
 function find2($table, $arg){
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
-    $pdo = new PDO($dsn, 'root', '');
+    // $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    // $pdo = new PDO($dsn, 'root', '');
+    $pdo=pdo();
     $sql = "select * from `$table` where ";
 
     if (is_array($arg)) {  //判斷$arg是否為陣列
@@ -169,8 +273,9 @@ function find2($table, $arg){
 
 //update()-給定資料表的條件後，會去更新相應的資料。
 function update($table, $cols, $id){
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
-    $pdo = new PDO($dsn, 'root', '');
+    // $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    // $pdo = new PDO($dsn, 'root', '');
+    $pdo=pdo();
     $sql = "update $table set $cols where `id`= $id";
     //$cols會是key=>value的形式>>用陣列
     //陣列拼成SQL語句>>設定變數$tmp
@@ -190,8 +295,9 @@ function update($table, $cols, $id){
 
 //insert()-給定資料內容後，會去新增資料到資料表
 function insert($table, $cols){
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
-    $pdo = new PDO($dsn, 'root', '');
+    // $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    // $pdo = new PDO($dsn, 'root', '');
+    $pdo=pdo();
     //cols為多筆資料>>用陣列
     $col = array_keys($cols); //用array_keys函數只取陣列的key值
     $sql = "insert into `$table`(`" . join("`,`", $col) . "`) value ('" . join("','", $cols) . "')";
@@ -204,8 +310,9 @@ function insert($table, $cols){
 
 //del()-給定條件後，會去刪除指定的資料
 function del($table, $id){
-    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
-    $pdo = new PDO($dsn, 'root', '');
+    // $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    // $pdo = new PDO($dsn, 'root', '');
+    $pdo=pdo();
     $sql = "delete from `$table` where `id` = '$id' ";
     $result = $pdo->exec($sql);
     return $result;
@@ -240,4 +347,10 @@ function save($table,$cols){
     }
 }
 
+//執行select較複雜的語法(可直接在function括弧內加入SQL語句)
+function q($sql){
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=vote3";
+    $pdo = new PDO($dsn, 'root', '');
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
